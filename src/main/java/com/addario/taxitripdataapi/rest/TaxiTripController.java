a5be18deb2002674046f4ca0597398cf32ceb17e
José Addario <Jose.Addario@pt.bosch.com>
2024-08-30 18:48:06 +0100

    Add upload controller

---
+++ src/main/java/com/addario/taxitripdataapi/rest/TaxiTripController.java
@@ -0,0 +1,11 @@
+package com.addario.taxitripdataapi.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1/api")
+public class TaxiTripController {
+
+
+}
